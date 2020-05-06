18bfd8ce47c2d39e24d45e5732287484b6ee0615
Chakshu Tandon <chakshutandon@gmail.com>
2020-05-06 04:37:01 +0000

    [UPDATE] qemu script to copy all project data

--- qemu/scripts/copy_data_to_qemu.sh
+++ qemu/scripts/copy_data_to_qemu.sh
@@ -4,9 +4,22 @@ set -x
 #Full path of directory or file to copy
 DIRPATH=$1
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+QEMU_DIR="$(dirname "$SCRIPT_DIR")"
+ROOT_DIR="$(dirname "$QEMU_DIR")"
+
 #Next, mount your image to the directory
 sudo mount -o loop $QEMU_IMG_FILE $MOUNT_DIR
 
-sudo cp -r $DIRPATH $MOUNT_DIR/root/
+if [ -z "$DIRPATH" ]
+then
+    sudo cp -r $ROOT_DIR/build $MOUNT_DIR/root/
+    sudo cp -r $ROOT_DIR/include $MOUNT_DIR/root/
+    sudo cp -r $ROOT_DIR/src $MOUNT_DIR/root/
+    sudo cp -r $ROOT_DIR/Makefile $MOUNT_DIR/root/
+    sudo cp -r $ROOT_DIR/bin $MOUNT_DIR/root/
+else
+    sudo cp -r $DIRPATH $MOUNT_DIR/root/
+fi
 
 sudo umount $MOUNT_DIR
